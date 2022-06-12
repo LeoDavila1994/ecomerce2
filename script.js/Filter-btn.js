647682c933902d271407b1e803b79e30ed57f5b7
LeoDavila1994 <liodavila.ld@gmail.com>
2022-06-12 02:31:05 -0500

    S52

---
+++ script.js/Filter-btn.js
@@ -0,0 +1,34 @@
+function filterbtn(){
+    const x = document.getElementById("Filtro");
+    x.classList.toggle("hidden");
+}
+
+function priceBtn(){
+    const x = document.getElementById("price-id");
+    x.classList.toggle("hidden-one");
+}
+
+function categoBtn(){
+    const x = document.getElementById("catego-id");
+    x.classList.toggle("hidden-two");
+}
+
+function dispoBtn(){
+    const x = document.getElementById("dispo-id");
+    x.classList.toggle("hidden-thre");
+}
+
+function ramBtn(){
+    const x = document.getElementById("ram-id");
+    x.classList.toggle("hidden-thre");
+}
+
+function menuBtn() {
+    const x = document.getElementById("menu-list");
+    x.classList.toggle("hidden-four");
+}
+
+function loginBtn() {
+    const x = document.getElementById("user-login");
+    x.classList.toggle("hidden-five");
+}
